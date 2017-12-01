4d0f482cbd90a80fe3c24ac8be20d5cf5817eb37
James Skinner <spiralx@gmail.com>
2017-12-01 04:04:42 +0000

    Added ngrx-store-logger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,35 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http'
 
-import { StoreModule } from '@ngrx/store'
+import { StoreModule, ActionReducer } from '@ngrx/store'
 import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+import { storeLogger } from 'ngrx-store-logger'
+
+// ----------------------------------------------------
+
+import { environment } from '../environments/environment'
 
 import { MaterialModule } from './material.module'
 
-import { ApiService } from './services/index'
-import { reducers } from './reducers/index'
+import { ApiService } from './services'
+import { reducers, State } from './reducers'
 
 import { AppComponent } from './app.component'
 import { SharePageComponent } from './containers/share-page.component'
 import { ShareTableComponent, ShareItemComponent } from './components/share-table.component'
 import { ShareInputComponent } from './components/share-input.component'
 import { DebugPanelComponent } from './components/debug-panel.component'
+
+// ----------------------------------------------------
+
+export function logger(reducer: ActionReducer<State>): any {
+  // default, no options
+  return storeLogger()(reducer)
+}
+
+export const metaReducers = environment.production
+  ? []
+  : [ logger ]
 
 // ----------------------------------------------------
 
@@ -27,7 +43,7 @@ import { DebugPanelComponent } from './components/debug-panel.component'
     FormsModule,
     HttpClientModule,
     MaterialModule,
-    StoreModule.forRoot(reducers),
+    StoreModule.forRoot(reducers, { metaReducers }),
     StoreDevtoolsModule.instrument({
       maxAge: 25
     })
